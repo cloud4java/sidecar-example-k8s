eaf9b5dd1db5aa1ca2641d255bda83d086b3fa90
cloud4java <dorival.querino@gmail.com>
2024-10-23 07:52:00 -0300

    adjust loca location, log reade with append mode

--- log-service/src/main/java/com/silva/logservice/service/LogHandler.java
+++ log-service/src/main/java/com/silva/logservice/service/LogHandler.java
@@ -5,30 +5,36 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 @Service
 public class LogHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(LogHandler.class);
-    public static final File LOG_FILE = new File("/logs/book-store.log");
+    public static final File LOG_FILE = new File("logs/book-store.log");
 
     @Scheduled(fixedRate = 5000)
     public void handleLogs() {
         LOG.info("Handling logs...");
+        long lastPosition = 0;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(LOG_FILE))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                //At this point,  the logs could be sent to ELK, Splunk, or other aggregator
-                LOG.info(line);
+        try (RandomAccessFile reader = new RandomAccessFile(LOG_FILE, "r")) {
+
+            long fileLength = LOG_FILE.length();
+            if(fileLength< lastPosition){
+                lastPosition = 0;
+            }
+            if(fileLength > lastPosition){
+                reader.seek(lastPosition);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    //At this point,  the logs could be sent to ELK, Splunk, or other aggregator
+                    LOG.info(line);
+                }
+                lastPosition = reader.getFilePointer();
             }
         } catch (IOException e) {
             LOG.error("An error occurred while reading the log file: ", e);
         }
-
     }
 }
